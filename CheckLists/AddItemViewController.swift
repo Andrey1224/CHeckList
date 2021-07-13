43ab209b57778f54d33b0d3a43a3eb954ea31e02
Andrey1224 <nepod77@gmail.com>
2021-07-12 21:06:22 -0400

    keep working

---
+++ CheckLists/AddItemViewController.swift
@@ -0,0 +1,84 @@
+//
+//  AddItemViewController.swift
+//  CheckLists
+//
+//  Created by Andrii Nepodymka on 7/12/21.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+      return nil
+    }
+
+    
+    
+    
+    
+    
+
+    @IBAction func cancel() {
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func done() {
+        
+        print("Contents of the text field: \(textField.text!)")
+
+          navigationController?.popViewController(animated: true)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      textField.becomeFirstResponder()
+    }
+
+    // MARK: - Text Field Delegates
+    func textField(
+      _ textField: UITextField,
+      shouldChangeCharactersIn range: NSRange,
+      replacementString string: String
+    ) -> Bool {
+      let oldText = textField.text!
+      let stringRange = Range(range, in: oldText)!
+      let newText = oldText.replacingCharacters(
+        in: stringRange,
+        with: string)
+      if newText.isEmpty {
+        doneBarButton.isEnabled = false
+      } else {
+        doneBarButton.isEnabled = true
+      }
+      return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+      doneBarButton.isEnabled = false
+      return true
+    }
+
+    
+    
+}
---
+++ CheckLists/Checklistitem.swift
@@ -0,0 +1,15 @@
+//
+//  Checklistitem.swift
+//  CheckLists
+//
+//  Created by Andrii Nepodymka on 7/12/21.
+//
+
+import Foundation
+
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
+
